76de64f76bb7fe1f3c7a242241930ca5d725f36a
KittenBO <79127518+KittenBO@users.noreply.github.com>
2024-11-13 17:28:23 +0700

    Delete server directory

--- server/routes/deviceRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import deviceController from "../controllers/deviceController.js";
-import checkRole from "../middleware/checkRoleMiddleware.js";
-export const deviceRouter = new Router();
-
-deviceRouter.post('/', checkRole('ADMIN'), deviceController.create )
-deviceRouter.get('/', deviceController.getAll )
-deviceRouter.get('/:id', deviceController.getOne )
--- server/routes/typeRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import typeController from "../controllers/typeController.js";
-import checkRole from "../middleware/checkRoleMiddleware.js";
-export const typeRouter = new Router();
-
-typeRouter.post('/', checkRole('ADMIN'), typeController.create )
-typeRouter.get('/', typeController.getAll )
