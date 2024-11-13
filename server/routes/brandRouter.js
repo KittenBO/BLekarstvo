7c5ba1d63b7f0f9c83ff266304fced7c953bdfbb
KittenBO <79127518+KittenBO@users.noreply.github.com>
2024-11-13 17:15:48 +0700

    Delete server directory

--- server/routes/brandRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import brandController from "../controllers/brandController.js";
-import checkRole from "../middleware/checkRoleMiddleware.js";
-
-export const brandRouter = new Router();
-
-brandRouter.post('/', checkRole('ADMIN'), brandController.create )
-brandRouter.get('/', brandController.getAll )
