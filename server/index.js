6e94b217d435b72e85393edb5c844b8af3c6bd6a
Server <kittenbot22@yandex.ru>
2025-03-09 09:36:20 +0000

    Ещё один баг испрв

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_API,
     credentials: true,
 }));
 app.use(express.static(path.resolve(__dirname, 'static')))
